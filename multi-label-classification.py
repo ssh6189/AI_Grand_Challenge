91ba21ca150081b08d3f06a947b0951add94328b
ssh6189 <ssh6189@naver.com>
2020-07-27 19:30:42 +0900

    AI_Grand_Challenge

---
+++ multi-label-classification.py
@@ -0,0 +1,268 @@
+from keras import models, layers
+from keras import Input
+from keras.models import Model, load_model
+from keras.preprocessing.image import ImageDataGenerator
+from keras import optimizers, initializers, regularizers, metrics
+from keras.callbacks import ModelCheckpoint, EarlyStopping
+from keras.layers import BatchNormalization, Conv2D, Activation, Dense, GlobalAveragePooling2D, MaxPooling2D, \
+    ZeroPadding2D, Add
+from sklearn.model_selection import train_test_split
+import random
+import os
+import cv2
+from keras.preprocessing.image import img_to_array
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+from sklearn.preprocessing import MultiLabelBinarizer
+folder_path = 'E:/AI-Challenge/'
+class_list = os.listdir(folder_path)
+img_list = []
+for cls in class_list :
+    tmp = os.listdir(folder_path+cls)
+    for i in range(len(tmp)):
+        img_list.append(folder_path+cls+'/'+tmp[i])
+imagePaths = sorted(img_list)
+#print(imagePaths)
+random.seed(42)
+random.shuffle(imagePaths)
+
+data = []
+labels = []
+
+for imagePath in imagePaths:
+    #print(imagePath)
+    image = cv2.imread(imagePath)
+    image = cv2.resize(image, (224, 224))
+    #print("OK")
+    image = img_to_array(image)
+    data.append(image)
+    cls = imagePath.split('/')
+    tmp = cls[-2]
+    real_cls = tmp.split('_')
+    labels.append(real_cls)
+
+data = np.array(data, dtype="float") / 255.0
+labels = np.array(labels)
+
+mlb = MultiLabelBinarizer()
+labels = mlb.fit_transform(labels)
+print(labels)
+MultiLabelBinarizer(classes=None, sparse_output=False)
+(trainX, testX, trainY, testY) = train_test_split(data,
+	labels, test_size=0.2, random_state=42)
+#print(trainX)
+
+# number of classes
+K = 8
+
+input_tensor = Input(shape=(224, 224, 3), dtype='float32', name='input')
+
+
+def conv1_layer(x):
+    x = ZeroPadding2D(padding=(3, 3))(x)
+    x = Conv2D(64, (7, 7), strides=(2, 2))(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    x = ZeroPadding2D(padding=(1, 1))(x)
+
+    return x
+
+
+def conv2_layer(x):
+    x = MaxPooling2D((3, 3), 2)(x)
+
+    shortcut = x
+
+    for i in range(3):
+        if (i == 0):
+            x = Conv2D(64, (1, 1), strides=(1, 1), padding='valid')(x)
+            x = BatchNormalization()(x)
+            x = Activation('relu')(x)
+
+            x = Conv2D(64, (3, 3), strides=(1, 1), padding='same')(x)
+            x = BatchNormalization()(x)
+            x = Activation('relu')(x)
+
+            x = Conv2D(256, (1, 1), strides=(1, 1), padding='valid')(x)
+            shortcut = Conv2D(256, (1, 1), strides=(1, 1), padding='valid')(shortcut)
+            x = BatchNormalization()(x)
+            shortcut = BatchNormalization()(shortcut)
+
+            x = Add()([x, shortcut])
+            x = Activation('relu')(x)
+
+            shortcut = x
+
+        else:
+            x = Conv2D(64, (1, 1), strides=(1, 1), padding='valid')(x)
+            x = BatchNormalization()(x)
+            x = Activation('relu')(x)
+
+            x = Conv2D(64, (3, 3), strides=(1, 1), padding='same')(x)
+            x = BatchNormalization()(x)
+            x = Activation('relu')(x)
+
+            x = Conv2D(256, (1, 1), strides=(1, 1), padding='valid')(x)
+            x = BatchNormalization()(x)
+
+            x = Add()([x, shortcut])
+            x = Activation('relu')(x)
+
+            shortcut = x
+
+    return x
+
+
+def conv3_layer(x):
+    shortcut = x
+
+    for i in range(4):
+        if (i == 0):
+            x = Conv2D(128, (1, 1), strides=(2, 2), padding='valid')(x)
+            x = BatchNormalization()(x)
+            x = Activation('relu')(x)
+
+            x = Conv2D(128, (3, 3), strides=(1, 1), padding='same')(x)
+            x = BatchNormalization()(x)
+            x = Activation('relu')(x)
+
+            x = Conv2D(512, (1, 1), strides=(1, 1), padding='valid')(x)
+            shortcut = Conv2D(512, (1, 1), strides=(2, 2), padding='valid')(shortcut)
+            x = BatchNormalization()(x)
+            shortcut = BatchNormalization()(shortcut)
+
+            x = Add()([x, shortcut])
+            x = Activation('relu')(x)
+
+            shortcut = x
+
+        else:
+            x = Conv2D(128, (1, 1), strides=(1, 1), padding='valid')(x)
+            x = BatchNormalization()(x)
+            x = Activation('relu')(x)
+
+            x = Conv2D(128, (3, 3), strides=(1, 1), padding='same')(x)
+            x = BatchNormalization()(x)
+            x = Activation('relu')(x)
+
+            x = Conv2D(512, (1, 1), strides=(1, 1), padding='valid')(x)
+            x = BatchNormalization()(x)
+
+            x = Add()([x, shortcut])
+            x = Activation('relu')(x)
+
+            shortcut = x
+
+    return x
+
+
+def conv4_layer(x):
+    shortcut = x
+
+    for i in range(6):
+        if (i == 0):
+            x = Conv2D(256, (1, 1), strides=(2, 2), padding='valid')(x)
+            x = BatchNormalization()(x)
+            x = Activation('relu')(x)
+
+            x = Conv2D(256, (3, 3), strides=(1, 1), padding='same')(x)
+            x = BatchNormalization()(x)
+            x = Activation('relu')(x)
+
+            x = Conv2D(1024, (1, 1), strides=(1, 1), padding='valid')(x)
+            shortcut = Conv2D(1024, (1, 1), strides=(2, 2), padding='valid')(shortcut)
+            x = BatchNormalization()(x)
+            shortcut = BatchNormalization()(shortcut)
+
+            x = Add()([x, shortcut])
+            x = Activation('relu')(x)
+
+            shortcut = x
+
+        else:
+            x = Conv2D(256, (1, 1), strides=(1, 1), padding='valid')(x)
+            x = BatchNormalization()(x)
+            x = Activation('relu')(x)
+
+            x = Conv2D(256, (3, 3), strides=(1, 1), padding='same')(x)
+            x = BatchNormalization()(x)
+            x = Activation('relu')(x)
+
+            x = Conv2D(1024, (1, 1), strides=(1, 1), padding='valid')(x)
+            x = BatchNormalization()(x)
+
+            x = Add()([x, shortcut])
+            x = Activation('relu')(x)
+
+            shortcut = x
+
+    return x
+
+
+def conv5_layer(x):
+    shortcut = x
+
+    for i in range(3):
+        if (i == 0):
+            x = Conv2D(512, (1, 1), strides=(2, 2), padding='valid')(x)
+            x = BatchNormalization()(x)
+            x = Activation('relu')(x)
+
+            x = Conv2D(512, (3, 3), strides=(1, 1), padding='same')(x)
+            x = BatchNormalization()(x)
+            x = Activation('relu')(x)
+
+            x = Conv2D(2048, (1, 1), strides=(1, 1), padding='valid')(x)
+            shortcut = Conv2D(2048, (1, 1), strides=(2, 2), padding='valid')(shortcut)
+            x = BatchNormalization()(x)
+            shortcut = BatchNormalization()(shortcut)
+
+            x = Add()([x, shortcut])
+            x = Activation('relu')(x)
+
+            shortcut = x
+
+        else:
+            x = Conv2D(512, (1, 1), strides=(1, 1), padding='valid')(x)
+            x = BatchNormalization()(x)
+            x = Activation('relu')(x)
+
+            x = Conv2D(512, (3, 3), strides=(1, 1), padding='same')(x)
+            x = BatchNormalization()(x)
+            x = Activation('relu')(x)
+
+            x = Conv2D(2048, (1, 1), strides=(1, 1), padding='valid')(x)
+            x = BatchNormalization()(x)
+
+            x = Add()([x, shortcut])
+            x = Activation('relu')(x)
+
+            shortcut = x
+
+    return x
+
+
+x = conv1_layer(input_tensor)
+x = conv2_layer(x)
+x = conv3_layer(x)
+x = conv4_layer(x)
+x = conv5_layer(x)
+
+x = GlobalAveragePooling2D()(x)
+output_tensor = Dense(K, activation='sigmoid')(x)
+classifier =Model(input_tensor, output_tensor)
+classifier.compile(optimizer=optimizers.SGD(lr = 0.01, momentum= 0.9, nesterov = True), loss='binary_crossentropy', metrics=['accuracy'])
+
+classifier.summary()
+BS = 16
+EPOCHS = 10
+trainX.reshape(-1,224,224,3)
+H = classifier.fit(trainX,trainY,validation_data=(testX, testY),steps_per_epoch=len(trainX) // BS,validation_steps=1,epochs=EPOCHS, verbose=1)
+
+proba = classifier.predict(trainX[1].reshape(1,224,224,3))
+
+pred = proba.copy()
+pred[pred>=0.5]=1
+pred[pred<0.5]=0
+print(pred)
